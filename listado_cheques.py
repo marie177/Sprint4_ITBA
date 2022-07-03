011ba4fc1a4b6138b8cf21b2d135b2ada61c9651
DevStin <torrecilla.agustin16@gmail.com>
2022-07-03 20:16:40 -0300

    agrego metodos de busqueda

--- listado_cheques.py
+++ listado_cheques.py
@@ -15,9 +15,44 @@ def leerCsv(path, argumentos):
     with open(path) as archivo:
           cheques = csv.DictReader(archivo)
           for cheque in cheques:
-            if cheque["DNI"] == argumentos[0]:
-                 print(cheque)      
+            if cheque["NroCheque"] == argumentos[0]:
+               print("usted busco por numero de cheque")
+               print(cheque) 
+
+    with open(path) as archivo:
+          cheques = csv.DictReader(archivo)
+          for cheque in cheques:
+            if cheque["CodigoBanco"] == argumentos[0]:
+               print("usted busco por Codigo de banco")
+               print(cheque) 
+
+    with open(path) as archivo:
+          cheques = csv.DictReader(archivo)
+          for cheque in cheques:
+            if cheque["CodigoScursal"] == argumentos[0]:
+               print("usted busco por codigo de sucursal")
+               print(cheque)      
  
+    with open(path) as archivo:
+          cheques = csv.DictReader(archivo)
+          for cheque in cheques:
+            if cheque["NumeroCuentaOrigen"] == argumentos[0]:
+               print("usted busco por numero de cuenta de origen")
+               print(cheque)      
+    with open(path) as archivo:
+          cheques = csv.DictReader(archivo)
+          for cheque in cheques:
+            if cheque["NumeroCuentaDestino"] == argumentos[0]:
+               print("usted busco por numero de cuenta de destino")
+               print(cheque)  
+                   
+    with open(path) as archivo:
+          cheques = csv.DictReader(archivo)
+          for cheque in cheques:
+            if cheque["DNI"] == argumentos[0]:
+               print("usted busco por numero de DNI")
+               print(cheque)  
+
 main()                
 
 #para probar, correr en la terminal: py listado_cheques.py test.csv 11580999
