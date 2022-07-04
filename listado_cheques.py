bc70d62af35a3e588cde6340175346ee405fe0e5
DevStin <torrecilla.agustin16@gmail.com>
2022-07-04 16:41:56 -0300

    agrego busqueda por dni y estado

--- listado_cheques.py
+++ listado_cheques.py
@@ -45,13 +45,24 @@ def leerCsv(path, argumentos):
             if cheque["NumeroCuentaDestino"] == argumentos[0]:
                print("usted busco por numero de cuenta de destino")
                print(cheque)  
-                   
+
     with open(path) as archivo:
           cheques = csv.DictReader(archivo)
           for cheque in cheques:
             if cheque["DNI"] == argumentos[0]:
                print("usted busco por numero de DNI")
-               print(cheque)  
+               print(cheque)
+
+    with open(path) as archivo:
+          cheques = csv.DictReader(archivo)
+          for cheque in cheques:
+            if cheque["Estado"] == argumentos[0]:
+              if cheque["DNI"] == argumentos[1]:
+                DNI = cheque["DNI"]
+                estado = cheque["Estado"]
+                print("Usted busco los cheques con estado", estado, "del DNI:", DNI )
+                print(cheque)
+ 
 
 main()                
 
