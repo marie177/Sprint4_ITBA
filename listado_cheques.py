9069f3d4a041a0af0c9634f9038993460a1e1422
B4N17 <vidalramiro052@gmail.com>
2022-07-05 00:23:19 -0300

    modifico como se muestra en consola la lista

--- listado_cheques.py
+++ listado_cheques.py
@@ -12,12 +12,15 @@ def main():
      
 def leerCsv(path, argumentos):
      
-    with open(path) as archivo:
+     with open(path) as archivo:
           cheques = csv.DictReader(archivo)
           for cheque in cheques:
-            if cheque["DNI"] == argumentos[0]:
-                 print(cheque)      
- 
-main()                
+               if cheque["DNI"] == argumentos[0]:
+                    print(f'Numero de cheque: {cheque["NroCheque"]}\nCodigo del banco {cheque["CodigoBanco"]}\nCodigo de sucursal: {cheque["CodigoScurusal"]}\nNumero de Cuenta de Origen: {cheque["NumeroCuentaOrigen"]}\nNumero de Cuenta de Destino: {cheque["NumeroCuentaDestino"]}\nValor Total: {cheque["Valor"]}\nFecha de Origen: {cheque["FechaOrigen"]}\nFecha de Pago: {cheque["FechaPago"]}\nDNI: {cheque["DNI"]}\nTipo: {cheque["Tipo"]}\nEstado: {cheque["Estado"]}\n')   
+print("***********************")
+print("Lista de Cheques")
+print("")
+main()
+print("***********************")                
 
 #para probar, correr en la terminal: py listado_cheques.py test.csv 11580999
