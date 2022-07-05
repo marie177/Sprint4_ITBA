21d3604ced0546be9f30ee010c2b9076b0bd73a7
Juan I Casareski <juan@snabb.cl>
2022-07-04 21:04:11 -0300

    Usar todos los argumentos a la vez

--- listado_cheques.py
+++ listado_cheques.py
@@ -1,69 +1,57 @@
 import csv
 import sys
-import os
+from datetime import datetime
+import time
 
-def main():
-     
-     PATH_TEST = sys.argv[1]
-     args = sys.argv[2:]
-     
-     leerCsv(PATH_TEST, args)
-     
-     
-def leerCsv(path, argumentos):
-     
-    with open(path) as archivo:
-          cheques = csv.DictReader(archivo)
-          for cheque in cheques:
-            if cheque["NroCheque"] == argumentos[0]:
-               print("usted busco por numero de cheque")
-               print(cheque) 
+# Por si se especifica estado
+if len(sys.argv) == 7:
+    RUTA_ARCHIVO = sys.argv[1]
+    DNI = sys.argv[2]
+    SALIDA = sys.argv[3]  
+    TIPO = sys.argv[4]
+    ESTADO = sys.argv[5]
+    F_INICIO, F_FIN = sys.argv[6].split(':')
+    
+# Por si no se especifica estado
+if len(sys.argv) == 6:
+    RUTA_ARCHIVO = sys.argv[1]
+    DNI = sys.argv[2]
+    SALIDA = sys.argv[3]  
+    TIPO = sys.argv[4]
+    ESTADO = None
+    F_INICIO, F_FIN = sys.argv[5].split(':')
 
-    with open(path) as archivo:
-          cheques = csv.DictReader(archivo)
-          for cheque in cheques:
-            if cheque["CodigoBanco"] == argumentos[0]:
-               print("usted busco por Codigo de banco")
-               print(cheque) 
 
-    with open(path) as archivo:
-          cheques = csv.DictReader(archivo)
-          for cheque in cheques:
-            if cheque["CodigoScursal"] == argumentos[0]:
-               print("usted busco por codigo de sucursal")
-               print(cheque)      
- 
-    with open(path) as archivo:
-          cheques = csv.DictReader(archivo)
-          for cheque in cheques:
-            if cheque["NumeroCuentaOrigen"] == argumentos[0]:
-               print("usted busco por numero de cuenta de origen")
-               print(cheque)      
-    with open(path) as archivo:
-          cheques = csv.DictReader(archivo)
-          for cheque in cheques:
-            if cheque["NumeroCuentaDestino"] == argumentos[0]:
-               print("usted busco por numero de cuenta de destino")
-               print(cheque)  
+with open(RUTA_ARCHIVO) as f:
+    cheques = csv.DictReader(f)
 
-    with open(path) as archivo:
-          cheques = csv.DictReader(archivo)
-          for cheque in cheques:
-            if cheque["DNI"] == argumentos[0]:
-               print("usted busco por numero de DNI")
-               print(cheque)
+    porDNI = list(filter(lambda cheque: cheque['DNI'] == DNI , cheques))
 
-    with open(path) as archivo:
-          cheques = csv.DictReader(archivo)
-          for cheque in cheques:
-            if cheque["Estado"] == argumentos[0]:
-              if cheque["DNI"] == argumentos[1]:
-                DNI = cheque["DNI"]
-                estado = cheque["Estado"]
-                print("Usted busco los cheques con estado", estado, "del DNI:", DNI )
-                print(cheque)
- 
+    porTipo = list(filter(lambda cheque: cheque['Tipo'] == TIPO , porDNI))
 
-main()                
+    if ESTADO:
+        # Lo guardo en porTipo para no tener que crear una variable que en algunas ocasiones no existe
+        porTipo = list(filter(lambda cheque: cheque['Estado'] == ESTADO , porTipo))
 
-#para probar, correr en la terminal: py listado_cheques.py test.csv 11580999
+    F_INICIO = datetime.strptime(F_INICIO, "%d-%m-%Y").timestamp()
+    F_FIN = datetime.strptime(F_FIN, "%d-%m-%Y").timestamp()
+
+    porFecha = list(filter(lambda cheque: F_INICIO < int(cheque['FechaOrigen']) < F_FIN, porTipo))
+    
+    if SALIDA == "PANTALLA":
+        print(porFecha)
+
+    if SALIDA == "CSV":
+        ahora = datetime.now().strftime('%d%m%y%y')
+        with open(f'{DNI}-{ahora}.csv', 'w') as f:
+            if porFecha:
+                f.write(', '.join(porFecha[0].keys()))
+                f.write('\n')
+
+            for cheque in porFecha:
+                f.write(', '.join(cheque.values()))
+                f.write('\n')
+
+
+# Para tests     
+# py main.py test.csv 11580999 CSV EMITIDO APROBADO 01-01-1990:10-10-2090
